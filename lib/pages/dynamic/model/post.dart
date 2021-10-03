3230b121fcb8fe2a27c72c85e81a7c122d8c1bf6
xiaqingpeng <626143872@qq.com>
2021-10-03 14:23:13 +0800

    ListView.builder

---
+++ lib/pages/dynamic/model/post.dart
@@ -0,0 +1,77 @@
+class Post {
+  const Post({
+    this.title,
+    this.author,
+    this.imageUrl,
+  });
+  final String title;
+  final String author;
+  final String imageUrl;
+}
+
+final List<Post> posts = [
+  Post(
+      title: '三国演义',
+      author: '罗贯中',
+      imageUrl: 'https://cms-dumall.cdn.bcebos.com/cms_upload_pro/cms_1633138913464.jpg?x-bce-process=image/quality,q_80/format,f_auto/interlace,i_progressive'),
+  Post(
+      title: '水浒传',
+      author: '施耐庵',
+      imageUrl: 'https://cms-dumall.cdn.bcebos.com/cms_upload_pro/cms_1633138913464.jpg?x-bce-process=image/quality,q_80/format,f_auto/interlace,i_progressive'),
+  Post(
+      title: '西游记',
+      author: '吴承恩',
+      imageUrl: 'https://cms-dumall.cdn.bcebos.com/cms_upload_pro/cms_1633138913464.jpg?x-bce-process=image/quality,q_80/format,f_auto/interlace,i_progressive'),
+  Post(
+      title: '红楼梦',
+      author: '曹雪芹',
+      imageUrl: 'https://cms-dumall.cdn.bcebos.com/cms_upload_pro/cms_1633138913464.jpg?x-bce-process=image/quality,q_80/format,f_auto/interlace,i_progressive'),
+  Post(
+      title: '三国演义',
+      author: '罗贯中',
+      imageUrl: 'https://cms-dumall.cdn.bcebos.com/cms_upload_pro/cms_1633138913464.jpg?x-bce-process=image/quality,q_80/format,f_auto/interlace,i_progressive'),
+  Post(
+      title: '水浒传',
+      author: '施耐庵',
+      imageUrl: 'https://cms-dumall.cdn.bcebos.com/cms_upload_pro/cms_1633138913464.jpg?x-bce-process=image/quality,q_80/format,f_auto/interlace,i_progressive'),
+  Post(
+      title: '西游记',
+      author: '吴承恩',
+      imageUrl: 'https://cms-dumall.cdn.bcebos.com/cms_upload_pro/cms_1633138913464.jpg?x-bce-process=image/quality,q_80/format,f_auto/interlace,i_progressive'),
+  Post(
+      title: '红楼梦',
+      author: '曹雪芹',
+      imageUrl: 'https://cms-dumall.cdn.bcebos.com/cms_upload_pro/cms_1633138913464.jpg?x-bce-process=image/quality,q_80/format,f_auto/interlace,i_progressive'),
+  Post(
+      title: '三国演义',
+      author: '罗贯中',
+      imageUrl: 'https://cms-dumall.cdn.bcebos.com/cms_upload_pro/cms_1633138913464.jpg?x-bce-process=image/quality,q_80/format,f_auto/interlace,i_progressive'),
+  Post(
+      title: '水浒传',
+      author: '施耐庵',
+      imageUrl: 'https://cms-dumall.cdn.bcebos.com/cms_upload_pro/cms_1633138913464.jpg?x-bce-process=image/quality,q_80/format,f_auto/interlace,i_progressive'),
+  Post(
+      title: '西游记',
+      author: '吴承恩',
+      imageUrl: 'https://cms-dumall.cdn.bcebos.com/cms_upload_pro/cms_1633138913464.jpg?x-bce-process=image/quality,q_80/format,f_auto/interlace,i_progressive'),
+  Post(
+      title: '红楼梦',
+      author: '曹雪芹',
+      imageUrl: 'https://cms-dumall.cdn.bcebos.com/cms_upload_pro/cms_1633138913464.jpg?x-bce-process=image/quality,q_80/format,f_auto/interlace,i_progressive'),
+  Post(
+      title: '三国演义',
+      author: '罗贯中',
+      imageUrl: 'https://cms-dumall.cdn.bcebos.com/cms_upload_pro/cms_1633138913464.jpg?x-bce-process=image/quality,q_80/format,f_auto/interlace,i_progressive'),
+  Post(
+      title: '水浒传',
+      author: '施耐庵',
+      imageUrl: 'https://cms-dumall.cdn.bcebos.com/cms_upload_pro/cms_1633138913464.jpg?x-bce-process=image/quality,q_80/format,f_auto/interlace,i_progressive'),
+  Post(
+      title: '西游记',
+      author: '吴承恩',
+      imageUrl: 'https://cms-dumall.cdn.bcebos.com/cms_upload_pro/cms_1633138913464.jpg?x-bce-process=image/quality,q_80/format,f_auto/interlace,i_progressive'),
+  Post(
+      title: '红楼梦',
+      author: '曹雪芹',
+      imageUrl: 'https://cms-dumall.cdn.bcebos.com/cms_upload_pro/cms_1633138913464.jpg?x-bce-process=image/quality,q_80/format,f_auto/interlace,i_progressive'),
+];
---
+++ lib/pages/dynamic/widget/listviewbuilder.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import '../model/post.dart';
+
+class ListViewBuilderPage extends StatelessWidget {
+  Widget itemBuilder(BuildContext context, int index) {
+    return Container(
+      child: Column(
+        children: [
+          Image.network(posts[index].imageUrl),
+          SizedBox(
+            height: 16.0,
+          ),
+          Text(
+            posts[index].title,
+            style: TextStyle( fontSize: 20.0,),
+          ),
+         
+          Text(
+            posts[index].author,
+            style: TextStyle( fontSize: 16.0,),
+          ),
+        ],
+      ),
+      alignment: Alignment.center,
+      margin: EdgeInsets.all(10.0),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('ListView.builder 控件'),
+          elevation: 10.0, // 阴影 默认4.0
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          // 对于从左到右的文本（TextDirection.ltr），文本从左向右流动；
+          // 对于从右到左的文本（TextDirection.rtl），文本从右向左流动。
+          child: ListView.builder(
+            itemBuilder: itemBuilder,
+            itemCount: posts.length,
+          ),
+        ),
+      ),
+    );
+  }
+}
